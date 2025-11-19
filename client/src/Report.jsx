63a4773f3b80454c7220da75d4edf272b21f445e
gswagatrmt <gswagatrmt@gmail.com>
2025-11-19 23:19:13 +0545

    Update Report.jsx

--- client/src/Report.jsx
+++ client/src/Report.jsx
@@ -312,7 +312,6 @@ export default function Report({ data }) {
                 justifyContent: "center",
                 alignItems: "flex-start",
                 margin: "0 auto",
-                maxHeight:"380px",
                 maxWidth: "205px",     // ✅ ensures it never looks oversized
               }}
             >
